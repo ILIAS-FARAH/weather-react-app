e833aba6eb83ddbe3ec4e016ebcbc2ad26fc3130
ILIAS-FARAH <iliasfarah5@gmail.com>
2025-02-23 17:11:19 +0000

    final-reslt

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/Logo.scss';
+
+
+function Logo() {
+  return (
+    <div className="logo-container">
+      <img src="/gif/cloudy.gif" alt="Cloudy GIF" className="logo-gif" />
+    </div>
+  );
+}
+
+export default Logo;
